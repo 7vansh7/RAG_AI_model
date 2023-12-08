b81421cbf0fedcb647f4a51fafb758a6c0766702
vansh aggarwal <vanshaggarwal@vanshs-MacBook-Pro.local>
2023-12-08 17:45:09 +0530

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-***This is a education app that is powered by a generative AI(ChatGPT in this case)***
+***This is a education app that is powered by a generative AI(Llama in this case)***
+
+1. git clone https://github.com/7vansh7/education_LLM.git
+2. Change the llama key to your own key
+3. source ./env/bin/activate
+4. python3 -m requirements.txt
+5. python3 main.py
--- main.py
+++ main.py
@@ -1,12 +1,46 @@
-from openai import OpenAI
-client = OpenAI()
+import json
+import os
+from llamaapi import LlamaAPI
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
-)
+# Initialize the llamaapi with your api_token
+llama = LlamaAPI(os.environ.get('KEY'))
 
-print(completion.choices[0].message)
+# Define your API request
+api_request_json = {
+    "messages": [
+        {"role": "assistant", "content": ""},
+    ],
+    # "functions": [
+    #     {
+    #         "name": "get_current_weather",
+    #         "description": "Get the current weather in a given location",
+    #         "parameters": {
+    #             "type": "object",
+    #             "properties": {
+    #                 "location": {
+    #                     "type": "string",
+    #                     "description": "The city and state, e.g. San Francisco, CA",
+    #                 },
+    #                 "days": {
+    #                     "type": "number",
+    #                     "description": "for how many days ahead you wants the forecast",
+    #                 },
+    #                 "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
+    #             },
+    #         },
+    #         "required": ["location", "days"],
+    #     }
+    # ],
+    # "stream": False,
+    # "function_call": "get_current_weather",
+}
+while True:
+    
+    content = input('\033[32m\033[4mGive your prompt\033[0m\033[0m: ')
+    if content == 'exit':
+        print('Bye👋')
+        break
+    api_request_json["messages"][0]['content'] = content
+    response = llama.run(api_request_json)
+    res = response.json()['choices'][0]['message']['content']
+    print(res)
