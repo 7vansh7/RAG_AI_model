38e58091e3d7306b779e010566b605d40d786f4c
vansh aggarwal <vanshaggarwal@vanshs-MacBook-Pro.local>
2023-12-20 17:11:10 +0530

    first version

--- backend.py
+++ backend.py
@@ -1,14 +1,20 @@
 import pinecone
 from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.document_loaders import TextLoader,PyPDFLoader
+from langchain.prompts import PromptTemplate
+from langchain.document_loaders import PyPDFLoader,TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.pinecone import Pinecone
 from llamaapi import LlamaAPI
 from langchain_experimental.llms import ChatLlamaAPI
 from langchain.chains.question_answering import load_qa_chain
+from langchain.chains.summarize import load_summarize_chain
 import os
 from dotenv import load_dotenv
 from tempfile import NamedTemporaryFile
+from langchain.memory import ConversationBufferMemory
+
+# Change the vectorbase from piencone to ChromaDB
+
 
 load_dotenv()
 model_name = "sentence-transformers/all-mpnet-base-v2"
@@ -32,12 +38,10 @@ def document_to_vector(document):
     with NamedTemporaryFile(delete=False) as tmp:  
         tmp.write(bytes_data)                      
         data = PyPDFLoader(tmp.name).load()        
-    # loader = PyPDFLoader(file)
-    # data = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
     texts = text_splitter.split_documents(data)
-    docsearch = Pinecone.from_texts([t.page_content for t in texts], hf, index_name='document-reader')
-    return docsearch
+    docs = Pinecone.from_texts([t.page_content for t in texts], hf, index_name='document-reader')
+    return docs
 
 
 def ques_to_answer(question):
@@ -54,5 +58,48 @@ def ques_to_answer(question):
     answer = chain.run(input_documents=docs, question=query)
     return answer
 
+def summarize(document):
 
-# working on the summarisation part of the app
+    llama = LlamaAPI(os.getenv('LLAMA_API_KEY'))
+    model = ChatLlamaAPI(client=llama)
+    txt_file = open('docs/file1.txt','r+')
+
+    file = document
+    bytes_data = file.read()
+    with NamedTemporaryFile(delete=False) as tmp:  
+        tmp.write(bytes_data)                      
+        data = PyPDFLoader(tmp.name)       
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=4000, chunk_overlap=400,separators=['\n','\n\n'])    
+    texts = data.load_and_split(text_splitter=text_splitter)
+    prompt = """
+    Write a summary with at least 10 points or as many points as you could of the following text delimited by triple backquotes.
+    Return your response in bullet points which covers the key points of the following text which is a collection of summaries.
+    ```{text}```
+    BULLET POINT SUMMARY:
+    """
+    prompt = PromptTemplate.from_template(prompt)
+    x,y = 0,9
+    for _ in range(int(len(texts)/9)):
+        prompt2 = """
+        Write a long summary for the following texts     
+        {text}
+        """
+        prompt = PromptTemplate.from_template(prompt)
+        chain = load_summarize_chain(model,chain_type='refine')
+        summary = chain.run(input_documents=texts[x:y],prompt=prompt2)
+        txt_file.write(summary + '\n')
+        x += 9
+        y += 9
+
+    final_docs = TextLoader('docs/file1.txt').load_and_split(text_splitter=text_splitter)
+
+    chain = load_summarize_chain(model,chain_type='map_reduce',map_reduce_prompt=prompt)
+    summary = chain.run(input_documents=final_docs)
+    return summary
+
+# the flow of the summarizer is -->
+# 1. Takes in the pdf
+# 2. Converts it into chunks
+# 3. summarizes all the chunks and adds them to a variable or a seperate text file
+# 4. Takes in all the summaries and then makes a combined summary from them all 
+# 5. Spits the combined summary
--- frontend.py
+++ frontend.py
@@ -4,28 +4,41 @@ import backend as B
 
 st.title(":rainbow[AI Professor] :pencil: ")
 
-st.write('Upload your document')
-with st.form("upload"):
-        document = st.file_uploader(label=' ')
-        st.form_submit_button('Upload')
-if document is not None:
-    B.document_to_vector(document)
-
 st.write('Task')
-task = st.selectbox('Pick your Task',['-','question-answering','summarisation'])
+task = st.selectbox('Pick your Task',['-','question-answering','summarization'])
 
 model = st.selectbox('Select the model',['llama2','chatgpt'])
 
 if task == 'question-answering':
-   with st.form("form"):
+   
+    st.write('Upload your document')
+    with st.form("upload"):
+        document = st.file_uploader(label=' ')
+        if st.form_submit_button('Upload'):
+            if document is not None:
+                B.document_to_vector(document)
+
+    with st.form("form"):
         st.write('Question')
         question = st.text_input('Write your question here')
-        st.form_submit_button('Answer')
-        answer = B.ques_to_answer(question)
-        st.write(answer)
+        if st.form_submit_button('Answer'):
+            answer = B.ques_to_answer(question)
+            st.write(answer)
 
-   
-
+if task == 'summarization' :
+        
+     
+        st.write('Upload your document')
+        with st.form("upload"):
+            document = st.file_uploader(label=' ')
+            btn =st.form_submit_button('Upload')
+        if document is not None:
+            with st.form('summarize'):   
+                if st.form_submit_button('Summarize the text'):
+                    with st.spinner('Converting...'):
+                        summary = B.summarize(document=document)
+                        st.write(summary)
+                    st.success('Done!!', icon="✅")
 
 
 
